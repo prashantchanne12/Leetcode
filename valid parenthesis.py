d70bd276ccd06fa5c7848c8ccad26737fd03d74d
prashantchanne12 <prashantchanne121@gmail.com>
2020-05-17 15:06:08 +0530

    valid parenthesis

---
+++ valid parenthesis.py
@@ -0,0 +1,67 @@
+'''
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+'''
+
+# Solution 1
+class Solution(object):
+    def isValid(self, s):
+        if s == '':
+            return True
+        stack=[]
+        for i in s:
+            if i in '{([':
+                stack.append(i)
+            elif i in '})]':
+                if i == ')' and len(stack) > 0 and stack[-1] == '(' :
+                    stack.pop()
+                elif i == '}' and len(stack) > 0 and stack[-1] == '{' :
+                    stack.pop()
+                elif i == ']' and len(stack) > 0 and stack[-1] == '[' :
+                    stack.pop()
+                else: 
+                    return False
+
+        return len(stack) == 0
+            
+
+# Solution 2
+class Solution(object):
+    def isValid(self, s):
+        dict = {'(':')','{':'}','[':']'}
+        stack = []
+        for i in range(0,len(s)):
+            if s[i] in '{[(':
+                stack.append(s[i])
+            elif len(stack)>0 and dict[stack[-1]]==s[i]:
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
