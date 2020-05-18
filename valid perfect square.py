255b5a61b55eebdbf63ca4dae8d00bf28bbe217f
prashantchanne12 <prashantchanne121@gmail.com>
2020-05-18 20:51:57 +0530

    valid perfect square

---
+++ valid perfect square.py
@@ -0,0 +1,20 @@
+'''
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+
+ 
+
+Example 1:
+
+Input: num = 16
+Output: true
+Example 2:
+
+Input: num = 14
+Output: false
+'''
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        return int(str(num**0.5).split('.')[-1]) == 0 
