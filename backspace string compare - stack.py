a3d6796de75e5f77e9a9d52de125a4d4f8e77a8b
prashantchanne12 <prashantchanne121@gmail.com>
2021-01-28 20:43:57 +0530

    backspace string compare - stack

---
+++ backspace string compare - stack.py
@@ -0,0 +1,44 @@
+'''
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+
+'''
+
+
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        def build(str):
+            arr = []
+
+            for i in str:
+                if i != '#':
+                    arr.append(i)
+                else:
+                    if arr:
+                        arr.pop()
+
+            return ''.join(arr)
+
+        return build(S) == build(T)
