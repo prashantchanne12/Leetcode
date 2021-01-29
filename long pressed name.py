63fe49bb9718d46569124bc2d0862c287e006437
prashantchanne12 <prashantchanne121@gmail.com>
2021-01-29 13:27:10 +0530

    long pressed name

---
+++ long pressed name.py
@@ -0,0 +1,53 @@
+'''
+Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
+
+ 
+
+Example 1:
+
+Input: name = "alex", typed = "aaleex"
+Output: true
+Explanation: 'a' and 'e' in 'alex' were long pressed.
+Example 2:
+
+Input: name = "saeed", typed = "ssaaedd"
+Output: false
+Explanation: 'e' must have been pressed twice, but it wasn't in the typed output.
+Example 3:
+
+Input: name = "leelee", typed = "lleeelee"
+Output: true
+Example 4:
+
+Input: name = "laiden", typed = "laiden"
+Output: true
+Explanation: It's not necessary to long press any character.
+'''
+
+
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
+
+        if len(name) > len(typed):
+            False
+
+        p1 = 0
+        p2 = 0
+
+        while p2 < len(typed):
+
+            if p1 < len(name) and name[p1] == typed[p2]:
+                p1 += 1
+            elif p2 == 0 or typed[p2] != typed[p2-1]:
+                return False
+
+            p2 += 1
+
+        return p1 == len(name)
