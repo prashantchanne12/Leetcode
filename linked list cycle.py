c23968656ac2451bf0527e4435816777bf72652f
prashantchanne12 <prashantchanne121@gmail.com>
2021-02-06 20:56:51 +0530

    linked list cycle

---
+++ linked list cycle.py
@@ -0,0 +1,39 @@
+'''
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Input: head = [3,2,0,-4] 3->2->0->->4
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Input: head = [3,2,0,-4] 3->2->0->->None
+Output: false
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True  # found the cycle
+
+        return False
--- move all negative numbers to one side of array.py
+++ move all negative numbers to one side of array.py
@@ -5,6 +5,8 @@ Examples :
 Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
 Output: -12 -13 -5 -7 -3 -6 11 6 5
 Note: Order of elements is not important here.
+
+GEEKFORGEEKS
 '''
 
 
