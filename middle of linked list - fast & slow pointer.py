a21ed4e44a41978b78cb3c5f7a461fca23253c24
prashantchanne12 <prashantchanne121@gmail.com>
2021-02-07 20:48:12 +0530

    middle of linked list - fast & slow pointer

---
+++ middle of linked list - fast & slow pointer.py
@@ -0,0 +1,49 @@
+'''
+Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+
+If the total number of nodes in the LinkedList is even, return the second middle node.
+
+Example 1:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> null
+Output: 3
+Example 2:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+Output: 4
+Example 3:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
+Output: 4
+'''
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+def find_middle_of_linked_list(head):
+    slow = head
+    fast = head
+
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+
+    print(find_middle_of_linked_list(head).value)
+
+
+main()
