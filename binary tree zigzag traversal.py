fcf2dc29388573fa10b082f5aaa91b99e7a3caf2
prashantchanne12 <prashantchanne121@gmail.com>
2021-03-10 16:38:33 +0530

    binary tree zigzag travsersal

---
+++ binary tree zigzag traversal.py
@@ -0,0 +1,62 @@
+'''
+Given a binary tree, populate an array to represent its zigzag level order traversal. You should populate the values of all nodes of the first level from left to right, then right to left for the next level and keep alternating in the same manner for the following levels.
+'''
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+
+
+def traverse(root):
+
+    res = []
+
+    if root is None:
+        return res
+
+    q = deque()
+    q.append(root)
+
+    while q:
+
+        currentLevel = deque()
+        levelSize = len(q)
+
+        for _ in range(levelSize):
+
+            currentNode = q.popleft()
+
+            if levelSize % 2 == 0:
+                currentLevel.appendleft(currentNode.val)
+            else:
+                currentLevel.append(currentNode.val)
+
+            if currentNode.left:
+                q.append(currentNode.left)
+
+            if currentNode.right:
+                q.append(currentNode.right)
+
+        res.append(list(currentLevel))
+
+    return res
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(17)
+    root.right = TreeNode(1)
+
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+
+    print(traverse(root))
+
+
+main()
