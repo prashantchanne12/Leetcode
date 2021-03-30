bf796400069a1ec5e3319a4f497132236444909e
prashantchanne12 <prashantchanne121@gmail.com>
2021-03-30 10:43:52 +0530

    updating tree diameter

--- tree diameter.py
+++ tree diameter.py
@@ -8,7 +8,7 @@ Note: You can always assume that there are at least two leaf nodes in the given 
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
-        self.left = left,
+        self.left = left
         self.right = right
 
 
@@ -27,16 +27,15 @@ class TreeDiameter:
             return 0
 
         leftTreeHeight = self.calculate_height(currentNode.left)
-
         rightTreeHeight = self.calculate_height(currentNode.right)
 
-        # diameter at the current node will be equal to the height of left subtree + the height of right subtree + '1' for the current node
+        # diameter at the current node will be equal to the height of the left subtree + the height of the right subtree + '1' for the current node
         diameter = leftTreeHeight + rightTreeHeight + 1
 
         # update the global tree diameter
         self.treeDiameter = max(self.treeDiameter, diameter)
 
-        # height of the current node will be equal to the maximum of both the heightd of left or right subtrees + '1' for the current node
+        # height of the current node will be equal to the maxinmum of the heights of left or right subtrees plus '1' for the current node
         return max(leftTreeHeight, rightTreeHeight) + 1
 
 
@@ -49,6 +48,15 @@ def main():
     root.left.left = TreeNode(4)
     root.right.left = TreeNode(5)
     root.right.right = TreeNode(6)
+
+    print(treeDiameter.find_diameter(root))
+
+    root.left.left = None
+    root.right.left.left = TreeNode(7)
+    root.right.left.right = TreeNode(8)
+    root.right.right.left = TreeNode(9)
+    root.right.left.right.left = TreeNode(10)
+    root.right.right.left.left = TreeNode(11)
 
     print(treeDiameter.find_diameter(root))
 
