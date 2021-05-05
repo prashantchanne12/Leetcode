622c817ffdbaa0e5715ea0244ef475a65ed4019e
poonam-bot7 <prashantchanne121@gmail.com>
2021-05-05 17:38:52 +0530

    new solution maximum sum subarray

--- maximum sum subarray.py
+++ maximum sum subarray.py
@@ -10,11 +10,33 @@ Follow up:
 
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 '''
+# Solution 1
+
+
 def maxSub(nums):
     max_current = max_global = nums[0]
-    for i in range(1,len(nums)):
+    for i in range(1, len(nums)):
         max_current = max(nums[i], max_current + nums[i])
         if max_current > max_global:
             max_global = max_current
 
     return max_global
+
+# Solution 2
+
+
+def max_sum_subarray(nums):
+    max_sum = 0
+    current_sum = 0
+
+    for i in range(0, len(nums)):
+        current_sum += nums[i]
+        max_sum = max(max_sum, current_sum)
+
+        if current_sum < 0:
+            current_sum = 0
+
+    return max_sum
+
+
+print(max_sum_subarray([5, -4, -2, 6, -1]))
