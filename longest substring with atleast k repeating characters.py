a965804c0bf654792c56f88b6f53c78455ba1643
poonam-bot7 <prashantchanne121@gmail.com>
2021-05-07 19:28:35 +0530

    longest substring with atleast k repeating characters

---
+++ longest substring with atleast k repeating characters.py
@@ -0,0 +1,46 @@
+'''
+Given a string s and an integer k, return the length of the longest substring of s such that the frequency of each character in this substring is greater than or equal to k.
+
+ 
+
+Example 1:
+
+Input: s = "aaabb", k = 3
+Output: 3
+Explanation: The longest substring is "aaa", as 'a' is repeated 3 times.
+Example 2:
+
+Input: s = "ababbc", k = 2
+Output: 5
+Explanation: The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+'''
+
+
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if len(s) == 0 or k > len(s):
+            return 0
+
+        freq_map = {}
+
+        for char in s:
+            if char not in freq_map:
+                freq_map[char] = 0
+
+            freq_map[char] += 1
+
+        sub1, sub2 = "", ""
+
+        for i, letter in enumerate(s):
+            if freq_map[letter] < k:
+                sub1 = self.longestSubstring(s[:i], k)
+                sub2 = self.longestSubstring(s[i+1:], k)
+                break
+        else:
+            return len(s)
+        return max(sub1, sub2)
