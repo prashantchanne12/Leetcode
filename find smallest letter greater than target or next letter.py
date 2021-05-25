cc784888005659f97c872b16e90d9d1c06461ed7
poonam-bot7 <prashantchanne121@gmail.com>
2021-05-25 12:18:44 +0530

    find smallest letter greater than target or next letter

---
+++ find smallest letter greater than target or next letter.py
@@ -0,0 +1,54 @@
+''''
+Given an array of lowercase letters sorted in ascending order, find the smallest letter in the given array greater than a given ‘key’.
+
+Assume the given array is a circular list, which means that the last letter is assumed to be connected with the first letter. This also means that the smallest letter in the given array is greater than the last letter of the array and is also the first letter of the array.
+
+Write a function to return the next letter of the given ‘key’.
+
+Example 1:
+
+Input: ['a', 'c', 'f', 'h'], key = 'f'
+Output: 'h'
+Explanation: The smallest letter greater than 'f' is 'h' in the given array.
+Example 2:
+
+Input: ['a', 'c', 'f', 'h'], key = 'b'
+Output: 'c'
+Explanation: The smallest letter greater than 'b' is 'c'.
+Example 3:
+
+Input: ['a', 'c', 'f', 'h'], key = 'm'
+Output: 'a'
+Explanation: As the array is assumed to be circular, the smallest letter greater than 'm' is 'a'.
+Example 4:
+
+Input: ['a', 'c', 'f', 'h'], key = 'h'
+Output: 'a'
+Explanation: As the array is assumed to be circular, the smallest letter greater than 'h' is 'a'
+''''
+
+
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+
+        low = 0
+        high = len(letters) - 1
+
+        if target < letters[low] or target > letters[high]:
+            return letters[low]
+
+        while low <= high:
+
+            mid = (low+high)//2
+
+            if target >= letters[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return letters[low % len(letters)]
