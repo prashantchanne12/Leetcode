c95e77c9457199cd1b165ad71c4fda56057a0db1
poonam-bot7 <prashantchanne121@gmail.com>
2021-05-29 20:59:50 +0530

    top k frequent elements updated

--- top k frequent elements.py
+++ top k frequent elements.py
@@ -39,3 +39,35 @@ class Solution(object):
 
 s = Solution()
 print(s.topKFrequent([1, 3, 5, 12, 11, 12, 11], 2))
+
+# Solution - 2
+
+from heapq import *
+
+def find_k_frequent_numbers(nums, k):
+    
+    dict = {}
+    
+    for num in nums:
+        if num not in dict:
+            dict[num] = 0
+            
+        dict[num] += 1
+
+    min_heap = []
+    
+    for key, val in dict.items():
+        heappush(min_heap, (val, key))
+        
+        if len(min_heap) > k:
+            heappop(min_heap)
+            
+    res = []
+    
+    while min_heap:
+        res.append(heappop(min_heap)[1])
+        
+    return res
+
+    
+print(find_k_frequent_numbers([1, 3, 5, 12, 11, 12, 11], 2))
