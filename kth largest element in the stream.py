81834c1e79c85a98a9e63ec2bd652d64b8136981
poonam-bot7 <prashantchanne121@gmail.com>
2021-05-29 21:24:01 +0530

    kth largest element in the stream

---
+++ kth largest element in the stream.py
@@ -0,0 +1,57 @@
+'''
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+'''
+from heapq import *
+
+
+class KthLargest(object):
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.k = k
+        self.min_heap = []
+
+        for num in nums:
+            self.add(num)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+
+        heappush(self.min_heap, val)
+
+        if len(self.min_heap) > self.k:
+            heappop(self.min_heap)
+
+        return self.min_heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
