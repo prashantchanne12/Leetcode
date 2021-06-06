490c1134f231efb46fbcc4a75fd329ddc0aa9761
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-06 21:42:11 +0530

    0/1 knapsack bottom up solution

--- 01 knapsack.py
+++ 01 knapsack.py
@@ -58,3 +58,28 @@ def knapsack_recursive_dp(profits, weights, capacity, n, dp):
 
 
 print(solve_knapsack([1, 6, 10], [1, 2, 3], 5))
+
+# Solution 3 - DP Bottom up approach
+
+
+def knapsack_bottom_up(profits, weights, capacity):
+
+    # create matrix
+    dp = [[0 if x == 0 or y == 0 else -
+           1 for x in range(0, capacity+1)] for y in range(0, len(profits)+1)]
+
+    for i in range(1, len(profits)+1):
+        for j in range(1, capacity+1):
+
+            if weights[i-1] <= j:
+                dp[i][j] = max(
+                    profits[i-1] + dp[i-1][j-weights[i-1]],
+                    dp[i-1][j]
+                )
+            else:
+                dp[i][j] = dp[i-1][j]
+
+    return dp[len(profits)][capacity]
+
+
+print(knapsack_bottom_up([20, 30, 10, 50], [1, 3, 4, 6], 10))
