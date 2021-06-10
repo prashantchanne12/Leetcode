b08296085ef6ed03ef519ebc0192758123c6b27a
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-10 17:59:49 +0530

    updated graph-bfs

--- graph-bfs.py
+++ graph-bfs.py
@@ -1,3 +1,6 @@
+# Solution 1
+
+
 def bfs(graph):
     queue = [0]
     values = []
@@ -21,3 +24,36 @@ def bfs(graph):
 
 graph = [[1, 3], [0], [3, 8], [0, 4, 5, 2], [3, 6], [3], [4, 7], [6], [2]]
 print(bfs(graph))
+
+# Solution - 2
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+
+def bfs(visited, graph, node):
+    visited = []
+    q = []
+
+    visited.append(node)
+    q.append(node)
+
+    while q:
+        node = q.pop(0)
+
+        for n in graph[node]:
+
+            if n not in visited:
+                visited.append(n)
+                q.append(n)
+
+    return visited
+
+
+print(bfs(visited, graph, 'A'))
