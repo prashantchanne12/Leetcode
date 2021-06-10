96e679b026587eb943524127994c24384fcccf8e
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-10 18:30:29 +0530

    udpated graph-dfs

--- graph-dfs.py
+++ graph-dfs.py
@@ -16,3 +16,28 @@ def dfs(vertex, graph, values, seen):
 
 graph = [[1, 3], [0], [3, 8], [0, 4, 5, 2], [3, 6], [3], [4, 7], [6], [2]]
 print(dfs(0, graph, [], {}))
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+arr = []
+
+
+def dfs(graph, visited, node):
+
+    if node not in visited:
+        visited.add(node)
+        arr.append(node)
+        for n in graph[node]:
+            dfs(graph, visited, n)
+
+
+visited = set()
+print(dfs(graph, visited, 'A'))
+print(arr)
