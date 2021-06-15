17dd346f86db4b1da8b975dcd7358b0030c7ce23
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-15 21:20:47 +0530

    sudoku solver

---
+++ sudoku solver.py
@@ -0,0 +1,144 @@
+'''
+Given a partially filled 9×9 2D array ‘grid[9][9]’, the goal is to assign digits (from 1 to 9) to the empty cells so that every row, column, and subgrid of size 3×3 contains exactly one instance of the digits from 1 to 9. 
+
+
+
+Example: 
+
+Input:
+grid = { {3, 0, 6, 5, 0, 8, 4, 0, 0}, 
+         {5, 2, 0, 0, 0, 0, 0, 0, 0}, 
+         {0, 8, 7, 0, 0, 0, 0, 3, 1}, 
+         {0, 0, 3, 0, 1, 0, 0, 8, 0}, 
+         {9, 0, 0, 8, 6, 3, 0, 0, 5}, 
+         {0, 5, 0, 0, 9, 0, 6, 0, 0}, 
+         {1, 3, 0, 0, 0, 0, 2, 5, 0}, 
+         {0, 0, 0, 0, 0, 0, 0, 7, 4}, 
+         {0, 0, 5, 2, 0, 6, 3, 0, 0} }
+Output:
+          3 1 6 5 7 8 4 9 2
+          5 2 9 1 3 4 7 6 8
+          4 8 7 6 2 9 5 3 1
+          2 6 3 4 1 5 9 8 7
+          9 7 4 8 6 3 1 2 5
+          8 5 1 7 9 2 6 4 3
+          1 3 8 9 4 7 2 5 6
+          6 9 2 3 5 1 8 7 4
+          7 4 5 2 8 6 3 1 9
+Explanation: Each row, column and 3*3 box of 
+the output matrix contains unique numbers.
+
+Input:    
+grid = { { 3, 1, 6, 5, 7, 8, 4, 9, 2 },
+         { 5, 2, 9, 1, 3, 4, 7, 6, 8 },
+         { 4, 8, 7, 6, 2, 9, 5, 3, 1 },
+         { 2, 6, 3, 0, 1, 5, 9, 8, 7 },
+         { 9, 7, 4, 8, 6, 0, 1, 2, 5 },
+         { 8, 5, 1, 7, 9, 2, 6, 4, 3 },
+         { 1, 3, 8, 0, 4, 7, 2, 0, 6 },
+         { 6, 9, 2, 3, 5, 1, 8, 7, 4 },
+         { 7, 4, 5, 0, 8, 6, 3, 1, 0 } };
+Output:
+           3 1 6 5 7 8 4 9 2 
+           5 2 9 1 3 4 7 6 8 
+           4 8 7 6 2 9 5 3 1 
+           2 6 3 4 1 5 9 8 7 
+           9 7 4 8 6 3 1 2 5 
+           8 5 1 7 9 2 6 4 3 
+           1 3 8 9 4 7 2 5 6 
+           6 9 2 3 5 1 8 7 4 
+           7 4 5 2 8 6 3 1 9 
+Explanation: Each row, column and 3*3 box of 
+the output matrix contains unique numbers.
+'''
+
+
+def find_empty_location(arr, l):
+
+    for row in range(9):
+        for col in range(9):
+
+            if (arr[row][col] == 0):
+
+                l[0] = row
+                l[1] = col
+
+                return True
+
+    return False
+
+
+def used_in_row(arr, row, num):
+
+    for i in range(9):
+        if arr[row][i] == num:
+            return True
+
+    return False
+
+
+def used_in_col(arr, col, num):
+
+    for i in range(9):
+        if arr[i][col] == num:
+            return True
+
+    return False
+
+
+def used_in_box(arr, row, col, num):
+    for i in range(3):
+        for j in range(3):
+            if arr[i + row][j + col] == num:
+                return True
+
+    return False
+
+
+def check_location_is_safe(arr, row, col, num):
+
+    return not used_in_row(arr, row, num) and not used_in_col(arr, col, num) and not used_in_box(arr, row - row % 3, col - col % 3, num)
+
+
+def solve_sudoku(arr):
+
+    l = [0, 0]
+
+    if not find_empty_location(arr, l):
+        return True
+
+    row, col = l[0], l[1]
+
+    for num in range(1, 10):
+
+        if check_location_is_safe(arr, row, col, num):
+
+            arr[row][col] = num
+
+            if (solve_sudoku(arr)):
+                return True
+
+            # backtrack
+            arr[row][col] = 0
+
+    return False
+
+
+# creating a 2D array for the grid
+grid = [[0 for x in range(9)]for y in range(9)]
+
+# assigning values to the grid
+grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+
+print(solve_sudoku(grid))
+for row in grid:
+    print(row)
