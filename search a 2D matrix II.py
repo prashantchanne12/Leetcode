0f9cadee2aafb59597099e95a985fdc8a7c90e00
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-17 16:19:53 +0530

    search a 2D matrix

---
+++ search a 2D matrix II.py
@@ -0,0 +1,42 @@
+'''
+Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+'''
+
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        row = 0
+        col = len(matrix[0]) - 1
+
+        while row < len(matrix) and col >= 0:
+
+            if matrix[row][col] == target:
+                return True
+
+            if target < matrix[row][col]:
+                col -= 1
+            else:
+                row += 1
+
+        return False
