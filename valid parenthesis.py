9ee687fb835eb845660c88d54f5f99fa22474b1a
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-21 20:04:34 +0530

    valid parenthesis update

--- valid parenthesis.py
+++ valid parenthesis.py
@@ -30,38 +30,69 @@ Output: true
 '''
 
 # Solution 1
+
+
 class Solution(object):
     def isValid(self, s):
         if s == '':
             return True
-        stack=[]
+        stack = []
         for i in s:
             if i in '{([':
                 stack.append(i)
             elif i in '})]':
-                if i == ')' and len(stack) > 0 and stack[-1] == '(' :
+                if i == ')' and len(stack) > 0 and stack[-1] == '(':
                     stack.pop()
-                elif i == '}' and len(stack) > 0 and stack[-1] == '{' :
+                elif i == '}' and len(stack) > 0 and stack[-1] == '{':
                     stack.pop()
-                elif i == ']' and len(stack) > 0 and stack[-1] == '[' :
+                elif i == ']' and len(stack) > 0 and stack[-1] == '[':
                     stack.pop()
-                else: 
+                else:
                     return False
 
         return len(stack) == 0
-            
+
 
 # Solution 2
 class Solution(object):
     def isValid(self, s):
-        dict = {'(':')','{':'}','[':']'}
+        dict = {'(': ')', '{': '}', '[': ']'}
         stack = []
-        for i in range(0,len(s)):
+        for i in range(0, len(s)):
             if s[i] in '{[(':
                 stack.append(s[i])
-            elif len(stack)>0 and dict[stack[-1]]==s[i]:
+            elif len(stack) > 0 and dict[stack[-1]] == s[i]:
                 stack.pop()
             else:
                 return False
+
+        return len(stack) == 0
+
+# Solution 3
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        if len(s) == 0:
+            return True
+
+        stack = []
+        dict = {
+            '(': ')',
+            '[': ']',
+            '{': '}',
+        }
+
+        for bracket in s:
+
+            if bracket in dict:
+                stack.append(bracket)
+            else:
+                if len(stack) > 0:
+                    if dict[stack.pop()] != bracket:
+                        return False
+                elif len(stack) == 0:
+                    return False
 
         return len(stack) == 0
