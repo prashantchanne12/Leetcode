3459341a28f9c0fcbe74f91ecd7bd29ffd3cac7c
poonam-bot7 <prashantchanne121@gmail.com>
2021-06-24 20:54:49 +0530

    flatten a multilevel doubly linked list

---
+++ flatten a multilevel doubly linked list.py
@@ -0,0 +1,82 @@
+'''
+You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
+
+Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the first level of the list.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+Explanation:
+
+The multilevel linked list in the input is as follows:
+
+
+
+After flattening the multilevel linked list it becomes:
+
+
+Example 2:
+
+Input: head = [1,2,null,3]
+Output: [1,3,2]
+Explanation:
+
+The input multilevel linked list is as follows:
+
+  1---2---NULL
+  |
+  3---NULL
+Example 3:
+
+Input: head = []
+Output: []
+'''
+"""
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+
+class Solution(object):
+    def flatten(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+
+        if head is None:
+            return head
+
+        temp = head
+
+        while head is not None:
+
+            if head.child is not None:
+
+                tail = head.child
+
+                while tail.next is not None:
+                    tail = tail.next
+
+                if head.next is not None:
+                    head.next.prev = tail
+                tail.next = head.next
+
+                head.child.prev = head
+                head.next = head.child
+
+                head.child = None
+
+            else:
+                head = head.next
+
+        return temp
