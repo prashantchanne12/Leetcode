743106f7430cfbbf65db8a6d0f0623d57a4f89b0
poonam-bot7 <prashantchanne121@gmail.com>
2021-07-07 12:12:13 +0530

    minimum operations to make array equal

--- majority elements.py
+++ majority elements.py
@@ -13,19 +13,34 @@ Input: [2,2,1,1,1,2,2]
 Output: 2
 '''
 
+
 class Solution(object):
-    def majorityElement(self,nums):
+    def majorityElement(self, nums):
         dict = {}
         for i in range(len(nums)):
             if nums[i] in dict:
                 dict[nums[i]] = dict.get(nums[i]) + 1
             else:
                 dict[nums[i]] = 1
-                
+
         for key, value in dict.items():
             if value > len(nums)/2:
                 return key
-        
-     
-    
-    
+
+
+def majority_element_2(nums):
+    ans_index = 0
+    count = 1
+
+    for i in range(1, len(nums)):
+
+        if nums[i] == nums[ans_index]:
+            count += 1
+        else:
+            count -= 1
+
+        if count == 0:
+            ans_index = i
+            count = 1
+
+    return nums[ans_index]
