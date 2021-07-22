73a0c7de48cc1182c5d21ae2e0d6c9d15c5a554d
poonam-bot7 <prashantchanne121@gmail.com>
2021-07-22 21:57:22 +0530

    intersection of two linked list

---
+++ intersection of two linked list.py
@@ -0,0 +1,88 @@
+'''
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+
+It is guaranteed that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+ 
+
+Example 1:
+
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+Example 2:
+
+
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+Example 3:
+
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+
+        len_a = 0
+        len_b = 0
+
+        temp_a = headA
+        temp_b = headB
+
+        while temp_a is not None:
+
+            len_a += 1
+            temp_a = temp_a.next
+
+        while temp_b is not None:
+
+            len_b += 1
+            temp_b = temp_b.next
+
+        diff = abs(len_a - len_b)
+
+        if len_a > len_b:
+
+            while diff:
+                headA = headA.next
+                diff -= 1
+
+        else:
+
+            while diff:
+                headB = headB.next
+                diff -= 1
+
+        while headA is not None and headB is not None:
+
+            if headA == headB:
+                return headA
+
+            headA = headA.next
+            headB = headB.next
+
+        return None
