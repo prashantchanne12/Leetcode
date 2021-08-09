bd00e9390767793dca95c2b95238026ff461bde1
poonam-bot7 <prashantchanne121@gmail.com>
2021-08-09 12:40:14 +0530

    median of two sorted arrays

--- median of two sorted arrays.py
+++ median of two sorted arrays.py
@@ -17,70 +17,43 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
 '''
 
-# Solution O(a+b)
 
-import numpy as np
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
 
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        final = []
-        i = 0
-        j = 0
-        
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                final.append(nums1[i])
-                i += 1
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        # we want B to be greater, A to be smaller
+        if len(B) < len(A):
+            B, A = A, B
+
+        left = 0
+        right = len(A) - 1
+
+        while True:
+
+            i = (left + right) // 2  # for A
+            # for B (i starts at 0, j starts at 0 thats why extra -2)
+            j = half - i - 2
+
+            A_left = A[i] if i >= 0 else float('-infinity')
+            A_right = A[i+1] if (i+1) < len(A) else float('infinity')
+
+            B_left = B[j] if j >= 0 else float('-infinity')
+            B_right = B[j+1] if (j+1) < len(B) else float('infinity')
+
+            # partition is correct
+            if A_left <= B_right and B_left <= A_right:
+                # odd
+                if total % 2:
+                    return min(A_right, B_right)
+
+                # even
+                return (max(A_left, B_left) + min(A_right, B_right)) / 2
+
+            elif A_left > B_right:
+                right = i - 1
             else:
-                final.append(nums2[j])
-                j += 1
-                
-        while i < len(nums1):
-            final.append(nums1[i])
-            i += 1
-            
-        while j < len(nums2):
-            final.append(nums2[j])
-            j += 1
-            
-        return np.median(final)
-
-# Solution O(n)
-
-import numpy as np
-
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        final = []
-        i = 0
-        j = 0
-        
-        while i < len(nums1) or j < len(nums2):
-            if i < len(nums1) and j < len(nums2):
-                if nums1[i] < nums2[j]:
-                    final.append(nums1[i])
-                    i += 1
-                else:
-                    final.append(nums2[j])
-                    j += 1
-                
-            elif i < len(nums1):
-                final.append(nums1[i])
-                i += 1
-                  
-            elif  j < len(nums2):
-                final.append(nums2[j])
-                j += 1
-                
-            
-        return np.median(final)
-
-
-# Solution 3 O(n) using python's sort
-import numpy as np
-
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        arr = nums1 + nums2
-        arr.sort()
-        return np.median(arr)
+                left = i + 1
