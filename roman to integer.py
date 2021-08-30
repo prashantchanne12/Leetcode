d8bf25bd6f0855c85e05a539547b641cf0deabba
poonam-bot7 <prashantchanne121@gmail.com>
2021-08-30 19:17:47 +0530

    roman to integer

--- roman to integer.py
+++ roman to integer.py
@@ -54,3 +54,31 @@ class Solution(object):
                 sum += dict[s[i]]
 
         return sum
+
+
+class Solution(object):
+    def romanToInt(self, s):
+
+        dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        res = 0
+        i = len(s) - 1
+
+        while i >= 0:
+
+            if i < len(s) - 1 and dict[s[i+1]] > dict[s[i]]:
+                res -= dict[s[i]]
+            else:
+                res += dict[s[i]]
+
+            i -= 1
+
+        return res
