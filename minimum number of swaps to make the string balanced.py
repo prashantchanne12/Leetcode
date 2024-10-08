fa6169ee9efc76b5d0eb153f2f4a284a39fa2905
Prashant Channe <prashantchanne121@gmail.com>
2024-10-08 14:23:17 -0400

    minimum number of swaps to make the string balanced

---
+++ minimum number of swaps to make the string balanced.py
@@ -0,0 +1,55 @@
+'''
+You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+
+A string is called balanced if and only if:
+
+It is the empty string, or
+It can be written as AB, where both A and B are balanced strings, or
+It can be written as [C], where C is a balanced string.
+You may swap the brackets at any two indices any number of times.
+
+Return the minimum number of swaps to make s balanced.
+
+ 
+
+Example 1:
+
+Input: s = "][]["
+Output: 1
+Explanation: You can make the string balanced by swapping index 0 with index 3.
+The resulting string is "[[]]".
+Example 2:
+
+Input: s = "]]][[["
+Output: 2
+Explanation: You can do the following to make the string balanced:
+- Swap index 0 with index 4. s = "[]][][".
+- Swap index 1 with index 5. s = "[[][]]".
+The resulting string is "[[][]]".
+Example 3:
+
+Input: s = "[]"
+Output: 0
+Explanation: The string is already balanced.
+ 
+'''
+
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        max = 0 # close brackets jiska pair nahi hai
+        close = 0
+
+        for char in s:
+            if char == "]":
+                close += 1
+
+                if max < close:
+                    max = close
+
+            if char == "[":
+                close -= 1
+
+        return (max + 1) // 2
+
+
+        
