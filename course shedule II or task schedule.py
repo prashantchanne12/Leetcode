6e527daf191f15ca782d0910ab54df74f5bcc269
Prashant Channe <prashantchanne121@gmail.com>
2024-11-26 22:50:51 -0500

    added different solution for course schedule II

--- course shedule II or task schedule.py
+++ course shedule II or task schedule.py
@@ -42,3 +42,52 @@ def is_scheduling_possible(vertices, edges):
 
 
 print(is_scheduling_possible(3, [[0, 1], [1, 2], [2, 0]]))
+
+# Solution - 2
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        prereq_dict = { i:[] for i in range(0, numCourses) }
+
+        for course, prereq in prerequisites:
+            prereq_dict[course].append(prereq)
+
+        
+        # A course has 3 possible states:
+        # visited -> course has been added to output
+        # visiting -> course not added to output, but added to cycle
+        # unvisited -> course not added to output or cycle
+
+        output = []
+        visited = set()
+        cycle = set()
+
+        def dfs(course):
+            if course in cycle:
+                return False
+
+            if course in visited:
+                return True
+
+            cycle.add(course)
+            
+            for pre in prereq_dict[course]:
+                if not dfs(pre):
+                    return False
+
+            cycle.remove(course)
+            visited.add(course)
+            output.append(course)
+
+            return True
+
+        for i in range(0, numCourses):
+            if not dfs(i):
+                return []
+            
+        return output
+
+
+
+        
